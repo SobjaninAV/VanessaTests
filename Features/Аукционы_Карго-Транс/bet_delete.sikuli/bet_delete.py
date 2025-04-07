4e5625a69d0f9f2f2c9eeb26e79bd8048958f47c
Anton Sobjanin <sobjaninav@gmail.com>
2025-04-07 18:13:05 +0500

    sikulix scripts added

---
+++ Features/Аукционы_Карго-Транс/bet_delete.sikuli/bet_delete.py
@@ -0,0 +1,11 @@
+from sikuli import *
+# Область экрана, на которой выполняется поиск элементов
+area = Region(1925,100,1913,937)
+# Путь к файлу изображений, которые нужно найти
+bet_delete = Pattern("bet_delete.png").targetOffset(0,6)
+reason_field = Pattern("reason_field.png").targetOffset(0,12)
+cancel_button = Pattern("cancel_button.png").targetOffset(0,6)
+# Кликаем изображение в указанной области
+click(area.find(bet_delete))
+type(area.find(reason_field), "Peredumal")
+click(area.find(cancel_button))
