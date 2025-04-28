c2fc02f744ac3d20a04b0941a2a3a9d9f145e766
Anton Sobjanin <sobjaninav@gmail.com>
2025-04-28 18:17:56 +0500

    minor changes
    Добавлены тесты на аукционы на понижение и фикс-прайс(карго-транс))

--- Siculix_scripts/bet_delete.sikuli/bet_delete.py
+++ Siculix_scripts/bet_delete.sikuli/bet_delete.py
@@ -3,10 +3,10 @@ from sikuli import *
 area = Region(1925,100,1913,937)
 # Путь к файлу изображений, которые нужно найти
 bet_delete = Pattern("bet_delete.png").targetOffset(0,6)
-reason_field = Pattern("reason_field.png").targetOffset(0,12)
+reason_field = Pattern("reason_field.png").targetOffset(0,18)
 cancel_button = Pattern("cancel_button.png").targetOffset(0,6)
 # Кликаем изображение в указанной области
 click(area.find(bet_delete))
 wait(1)
-type(area.find(reason_field), "Peredumal")
+type(area.find(reason_field), "nuzhno bol'she zolota")
 click(area.find(cancel_button))
