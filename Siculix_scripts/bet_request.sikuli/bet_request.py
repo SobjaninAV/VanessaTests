cb4acfb14240ebe62cc77ce3fe0c3689d5972a8e
Anton Sobjanin <sobjaninav@gmail.com>
2025-04-29 18:17:25 +0500

    Добавлены встречные ставки
    Добавлены встречные ставки в аукционы на понижение и голландский

--- Siculix_scripts/bet_request.sikuli/bet_request.py
+++ Siculix_scripts/bet_request.sikuli/bet_request.py
@@ -3,7 +3,6 @@ from sikuli import *
 area = Region(1925,100,1913,937)
 # Путь к файлу изображений, которые нужно найти
 bet_request_field = Pattern("bet_request_field.png").targetOffset(-39,2)
-reason_field = Pattern("reason_field.png").targetOffset(0,18)
 # Кликаем изображение в указанной области
 click(area.find(bet_request_field))
 wait(1)
